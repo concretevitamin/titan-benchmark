43dcb9a59eb41a22268f9d0ed6ff357f2ab690c3
Zongheng Yang <zongheng.y@gmail.com>
2015-09-18 15:45:21 -0700

    Minor path fix

--- par-load-node.sh
+++ par-load-node.sh
@@ -5,6 +5,6 @@ nodeSplits=( FIXME FIXME )
 export MAVEN_OPTS="-server -Xmx100g"
 
 mvn exec:java \
-  -Dexec.mainClass="edu.berkeley.cs.benchmark.ParLoadNode" \
+  -Dexec.mainClass="edu.berkeley.cs.ParLoadNode" \
   -Dexec.args="${nodeSplits[*]}" \
   2>&1 >parNode.log
